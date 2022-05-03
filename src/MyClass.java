00444562969ea296a0460a0385fd062405198c58
Yesudas <kunchanapalliyesudas@gmail.com>
2022-05-03 12:18:23 +0530

    Initial Commit

--- src/MyClass.java
+++ src/MyClass.java
@@ -17,46 +17,14 @@ public class MyClass
 
     public static void main(String[] args)
     {
-        int beachId=0;
-        String beachName=null;
-        String location=null;
-        String beachType=null;
-        int rating=0;
-        int avgVisitorsPerDay=0;
-
-        Beach beache=new Beach(beachId,beachName,location,beachType,rating,avgVisitorsPerDay);
-
         Scanner scanner=new Scanner(System.in);
         Beach[] beaches=new Beach[4];
 
         for(int i=0;i<4;i++)
         {
-            beachId=scanner.nextInt();
-            beaches[i]=beache;
-            beache.setBeachId(beachId);
-
-            beachName=scanner.next();
-            beaches[i]=beache;
-            beache.setBeachName(beachName);
-
-            location=scanner.next();
-            beaches[i]=beache;
-            beache.setLocation(location);
-
-            beachType=scanner.next();
-            beaches[i]=beache;
-            beache.setBeachType(beachType);
-
-            rating=scanner.nextInt();
-            beaches[i]=beache;
-            beache.setRating(rating);
-
-            avgVisitorsPerDay=scanner.nextInt();
-            beaches[i]=beache;
-            beache.setAvgVisitorsPerDay(avgVisitorsPerDay);
-
-            beaches[i]=new Beach(beachId,beachName,location,beachType,rating,avgVisitorsPerDay);
+            beaches[i]=new Beach(scanner.nextInt(),scanner.next(),scanner.next(),scanner.next(),scanner.nextInt(),scanner.nextInt());
         }
+
         int res=MyClass.findBeachWithLowestRating(beaches);
         System.out.println(res);
     }
